b3c8d8eac2694bf9b472b8622be7dce29fccfe0c
SinChaNo <atom7667@naver.com>
2023-06-12 18:24:08 +0900

    main, deleteBtn, post add email

--- app/page.js
+++ app/page.js
@@ -6,20 +6,9 @@ import SignIn from './signIn/page';
 
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  let client = await connectDB;
-  const db = client.db('forum');
-  let result = await db.collection('post').find().toArray();
-
-  console.log(result)
   return (
     <>
-      {!session ? 
-        <SignIn />
-        : 
-        <Hero />
-      }
+      <Hero />
     </>
   )
 }
--- pages/api/post.js
+++ pages/api/post.js
@@ -1,6 +1,22 @@
-const post = (res, req) => {
-  if (res.method == 'GET'){
-    return(req.status(200).json("POST API"))
+import { connectDB } from "@/mongoDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+
+const post = async (res, req) => {
+  let session =  await getServerSession(res, req, authOptions);
+  console.log(session);
+  if (session) {
+    res.body.author = session.user.email
+  }
+  if (res.method == 'POST'){
+    const doc = res.body;
+    console.log(doc)
+    const db = (await connectDB).db('forum');
+    let result = await db.collection('post').insertOne(doc);
+    console.log(
+      `A document was inserted with the _id: ${result.insertedId}`,
+    );
+    return req.status(200).redirect(302,'/main');
   }
 }
 
